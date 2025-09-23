9ef97f23941861ac37ad1daad824409de9dc68eb
katsuki.ito <fordevelopment10@gmail.com>
2025-09-23 20:29:48 +0900

    chore: bootstrap krs-identity-gw scaffolding

---
+++ src/main/java/dev/kairos/krsidentitygw/KrsIdentityGwApplication.java
@@ -0,0 +1,13 @@
+package dev.kairos.krsidentitygw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrsIdentityGwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrsIdentityGwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/kairos/krsidentitygw/KrsIdentityGwApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kairos.krsidentitygw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrsIdentityGwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
